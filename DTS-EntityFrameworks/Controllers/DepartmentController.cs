dfb00c5b21b88856c5fd0b877ba4c5d7cd6e6146
Andhika Pramana <andhikapramana101@gmail.com>
2022-09-26 13:54:03 +0700

    Add & Modify CRUD Feature for Department, Division, & Region

--- DTS-EntityFrameworks/Controllers/DepartmentController.cs
+++ DTS-EntityFrameworks/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using DTS_EntityFrameworks.Context;
+using DTS_EntityFrameworks.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +19,111 @@ namespace DTS_EntityFrameworks.Controllers
         }
 
 
-        
+        //READ
+        //GET
         public IActionResult Index()
         {
+            var data = myContext.Departments.Include(x=>x.Division).ToList();
+            return View(data);
+        }
+
+        //READ BY ID
+        //GET
+        public IActionResult Detail(int Id)
+        {
+            var data = myContext.Departments
+                .Where(x=>x.Id==Id)
+                .Include(x => x.Division);
+            return View(data);
+        }
+
+
+        //CREATE
+        //GET
+        public IActionResult Create()
+        {
+            //var data = myContext.Divisions.ToList();
+            //ViewBag.Divisions = data;
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Departments.Add(department);
+                if (myContext.SaveChanges()>0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        //UPDATE
+        //GET
+        public IActionResult Edit(int Id)
+        {
+            var data = myContext.Departments
+               .Where(x => x.Id == Id)
+               .Include(x => x.Division);
+            return View(data);
+        }
+    
+        //POST
+        public IActionResult Edit(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Departments.Update(department);
+                if (myContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        //DELETE
+        //GET
+        public IActionResult Delete(int Id)
+        {
+            var data = myContext.Departments
+               .Where(x => x.Id == Id)
+               .Include(x => x.Division);
+            return View(data);
+        }
+        //POST
+        public IActionResult Delete(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Departments.Remove(department);
+                if (myContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+        
+        
+
+
     }
 }
---
+++ DTS-EntityFrameworks/Controllers/DivisionController.cs
@@ -0,0 +1,126 @@
+﻿using DTS_EntityFrameworks.Context;
+using DTS_EntityFrameworks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTS_EntityFrameworks.Controllers
+{
+    public class DivisionController : Controller
+    {
+        MyContext myContext;
+
+        public DivisionController(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+
+
+        //READ
+        //GET
+        public IActionResult Index()
+        {
+            var data = myContext.Divisions.ToList();
+            return View(data);
+        }
+
+        //READ BY ID
+        //GET
+        public IActionResult Detail(int Id)
+        {
+            var data = myContext.Divisions
+                .Where(x => x.Id == Id);
+            return View(data);
+        }
+
+
+        //CREATE
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Divisions.Add(division);
+                if (myContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        //UPDATE
+        //GET
+        public IActionResult Edit(int Id)
+        {
+            var data = myContext.Divisions
+               .Where(x => x.Id == Id);
+            return View(data);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Divisions.Update(division);
+                if (myContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        //DELETE
+        //GET
+        public IActionResult Delete(int Id)
+        {
+            var data = myContext.Divisions
+               .Where(x => x.Id == Id);
+            return View(data);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Divisions.Remove(division);
+                if (myContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+
+    }
+}
