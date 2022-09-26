9334114c5fcf51ee609dab67e1cdf82e5e976a17
Andhika Pramana <andhikapramana101@gmail.com>
2022-09-26 10:48:27 +0700

    Initial

---
+++ DTS-EntityFrameworks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DTS_EntityFrameworks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
